b81d8669b91f86f34009099ce1886cca56044542
Artchi97 <artur.wojciechowski97@gmail.com>
2024-11-22 07:58:54 +0100

    some UX improvements

--- src/components/MainImg.tsx
+++ src/components/MainImg.tsx
@@ -10,14 +10,16 @@ const MainImg: React.FC = () => {
 
   return (
     <div
-      key={language}
       style={{ backgroundImage: `url(${mainImg})` }}
       className="bg-cover bg-center w-full h-60 md:h-96 mt-8 sm:mt-0 relative"
     >
       <div className="absolute w-full h-full bg-black opacity-35"></div>
 
       <div className="flex items-center justify-center absolute w-full h-full px-8 xl:px-12">
-        <p className="font-oswald font-semibold italic animate-pulse-text text-xl md:text-3xl lg:text-5xl  text-center leading-[1.4] xl:leading-[1.5] bg-gradient-to-r from-white to-yellow-100 text-transparent bg-clip-text">
+        <p
+          key={language}
+          className="font-oswald font-semibold italic animate-pulse-text text-xl md:text-3xl lg:text-5xl  text-center leading-[1.4] xl:leading-[1.5] bg-gradient-to-r from-white to-yellow-100 text-transparent bg-clip-text"
+        >
           {translations.mainTitle}
         </p>
       </div>
